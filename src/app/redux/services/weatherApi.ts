a94a447b0e8e3080685656480573da403cd06c47
Rafael Monteiro Greco <72089301+Rmgreco@users.noreply.github.com>
2023-06-27 01:47:02 -0300

    Removes keys

--- src/app/redux/services/weatherApi.ts
+++ src/app/redux/services/weatherApi.ts
@@ -12,7 +12,7 @@ export const weatherApi = createApi({
   }),
   endpoints: (builder) => ({
     getWeather: builder.query<WeatherCardProps, string>({
-      query: (city) => `weather?q=${city}&appid=0bfd1f48da1715cefa9cc403a456bd5b`,
+      query: (city) => `weather?q=${city}&appid=`,
     }),
   }),
 });
@@ -24,7 +24,7 @@ export const geolocationApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.opencagedata.com/geocode/v1/' }),
   endpoints: (builder) => ({
     getCurrentLocation: builder.query<string, { lat: number; lng: number }>({
-      query: ({ lat, lng }) => `json?q=${lat}+${lng}&key=23d23199932041c5a9f9ce2abef4678c`,
+      query: ({ lat, lng }) => `json?q=${lat}+${lng}&key=`,
     }),
   }),
 });
